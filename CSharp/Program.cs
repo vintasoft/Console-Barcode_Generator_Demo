dd3e2b8b7c16aaa69dc2a508dd2d3a8aa65e93e0
VintaSoft <alex@vintasoft.com>
2019-04-07 16:38:08 +0300

    Add files via upload

---
+++ CSharp/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Drawing;
+using Vintasoft.Barcode;
+
+namespace BarcodeGeneratorConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Usage: BarcodeGeneratorConsoleDemo outputfile.png BarcodeType BarcodeValue");
+                    return;
+                }
+                try
+                {
+                    // register custom encodings for QR and HanXin Code barcodes 
+                    // (System.Text.Encoding.CodePages package)
+                    System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+
+                    // create barcode writer
+                    BarcodeWriter writer = new BarcodeWriter();
+                    writer.Settings.PixelFormat = BarcodeImagePixelFormat.Bgr24;
+
+                    string fileName = args[0];
+                    string barcodeType = args[1];
+                    string barcodeValue = args[2];
+
+                    // set type of generating barcode
+                    SetBarcodeType(writer.Settings, barcodeType);
+
+                    // specify barcode value
+                    writer.Settings.Value = barcodeValue;
+
+                    // generate barcode as image
+                    using (Bitmap bitmap = writer.GetBarcodeAsBitmap())
+                    {
+                        // save barcode image
+                        bitmap.Save(fileName);
+
+                        Console.WriteLine(string.Format("File '{0}' is created.", fileName));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("Error: {0}", ex.Message));
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Read any key...");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Sets barcode type by name.
+        /// </summary>
+        /// <param name="settings">The barcode writer settings.</param>
+        /// <param name="name">The name of barcode type.</param>
+        private static void SetBarcodeType(WriterSettings settings, string name)
+        {
+            switch (name)
+            {
+                case "AustralianPost":
+                    settings.Barcode = BarcodeType.AustralianPost;
+                    break;
+                case "Aztec":
+                    settings.Barcode = BarcodeType.Aztec;
+                    break;
+                case "Codabar":
+                    settings.Barcode = BarcodeType.Codabar;
+                    break;
+                case "Code11":
+                    settings.Barcode = BarcodeType.Code11;
+                    break;
+                case "Code128":
+                    settings.Barcode = BarcodeType.Code128;
+                    break;
+                case "Code16K":
+                    settings.Barcode = BarcodeType.Code16K;
+                    break;
+                case "Code39":
+                    settings.Barcode = BarcodeType.Code39;
+                    break;
+                case "Code93":
+                    settings.Barcode = BarcodeType.Code93;
+                    break;
+                case "DataMatrix":
+                    settings.Barcode = BarcodeType.DataMatrix;
+                    break;
+                case "DutchKIX":
+                    settings.Barcode = BarcodeType.DutchKIX;
+                    break;
+                case "EAN13":
+                    settings.Barcode = BarcodeType.EAN13;
+                    break;
+                case "EAN13Plus2":
+                    settings.Barcode = BarcodeType.EAN13Plus2;
+                    break;
+                case "EAN13Plus5":
+                    settings.Barcode = BarcodeType.EAN13Plus5;
+                    break;
+                case "EAN8":
+                    settings.Barcode = BarcodeType.EAN8;
+                    break;
+                case "EAN8Plus2":
+                    settings.Barcode = BarcodeType.EAN8Plus2;
+                    break;
+                case "EAN8Plus5":
+                    settings.Barcode = BarcodeType.EAN8Plus5;
+                    break;
+                case "HanXinCode":
+                    settings.Barcode = BarcodeType.HanXinCode;
+                    break;
+                case "IATA2of5":
+                    settings.Barcode = BarcodeType.IATA2of5;
+                    break;
+                case "IntelligentMail":
+                    settings.Barcode = BarcodeType.IntelligentMail;
+                    break;
+                case "Interleaved2of5":
+                    settings.Barcode = BarcodeType.Interleaved2of5;
+                    break;
+                case "Mailmark4StateC":
+                    settings.Barcode = BarcodeType.Mailmark4StateC;
+                    break;
+                case "Mailmark4StateL":
+                    settings.Barcode = BarcodeType.Mailmark4StateL;
+                    break;
+                case "Matrix2of5":
+                    settings.Barcode = BarcodeType.Matrix2of5;
+                    break;
+                case "MaxiCode":
+                    settings.Barcode = BarcodeType.MaxiCode;
+                    break;
+                case "MicroPDF417":
+                    settings.Barcode = BarcodeType.MicroPDF417;
+                    break;
+                case "MicroQR":
+                    settings.Barcode = BarcodeType.MicroQR;
+                    break;
+                case "MSI":
+                    settings.Barcode = BarcodeType.MSI;
+                    break;
+                case "PDF417":
+                    settings.Barcode = BarcodeType.PDF417;
+                    break;
+                case "PDF417Compact":
+                    settings.Barcode = BarcodeType.PDF417Compact;
+                    break;
+                case "PatchCode":
+                    settings.Barcode = BarcodeType.PatchCode;
+                    break;
+                case "Pharmacode":
+                    settings.Barcode = BarcodeType.Pharmacode;
+                    break;
+                case "Planet":
+                    settings.Barcode = BarcodeType.Planet;
+                    break;
+                case "Postnet":
+                    settings.Barcode = BarcodeType.Postnet;
+                    break;
+                case "QR":
+                    settings.Barcode = BarcodeType.QR;
+                    break;
+                case "RoyalMail":
+                    settings.Barcode = BarcodeType.RoyalMail;
+                    break;
+                case "RSS14":
+                    settings.Barcode = BarcodeType.RSS14;
+                    break;
+                case "RSS14Stacked":
+                    settings.Barcode = BarcodeType.RSS14Stacked;
+                    break;
+                case "RSSExpanded":
+                    settings.Barcode = BarcodeType.RSSExpanded;
+                    break;
+                case "RSSExpandedStacked":
+                    settings.Barcode = BarcodeType.RSSExpandedStacked;
+                    break;
+                case "RSSLimited":
+                    settings.Barcode = BarcodeType.RSSLimited;
+                    break;
+                case "Standard2of5":
+                    settings.Barcode = BarcodeType.Standard2of5;
+                    break;
+                case "Telepen":
+                    settings.Barcode = BarcodeType.Telepen;
+                    break;
+                case "UPCA":
+                    settings.Barcode = BarcodeType.UPCA;
+                    break;
+                case "UPCAPlus2":
+                    settings.Barcode = BarcodeType.UPCAPlus2;
+                    break;
+                case "UPCAPlus5":
+                    settings.Barcode = BarcodeType.UPCAPlus5;
+                    break;
+                case "UPCE":
+                    settings.Barcode = BarcodeType.UPCE;
+                    break;
+                case "UPCEPlus2":
+                    settings.Barcode = BarcodeType.UPCEPlus2;
+                    break;
+                case "UPCEPlus5":
+                    settings.Barcode = BarcodeType.UPCEPlus5;
+                    break;
+                default:
+                    throw new NotSupportedException(string.Format("Barcode type '{0}' is undefined.", name));
+            }
+        }
+    }
+}
